408bae92fee0f00a8368906d396154bf3da7c167
christyuda <chrisyuda@ulbi.ac.id>
2023-08-24 22:48:37 +0700

    fix it section advance function aysnc await

--- 05_Advance Function_Async_ Await _Collection_B/Praktikum/soaleksplorasi.dart
+++ 05_Advance Function_Async_ Await _Collection_B/Praktikum/soaleksplorasi.dart
@@ -1,14 +1,29 @@
 
 //no 1 duplikat dari list 
 List<String> inputList =['amuse', 'tommy kaira','spoon','HKS','litchfield','amuse','HKS'];
-List<String> listUnik = inputList.toSet().toList();
+List<String> listUnik = removeDuplicates(inputList);
 
+  List<String> inputlist2 = ['JS Racing', 'amuse', 'spoon', 'spoon', 'JS Racing', 'amuse'];
+  List<String> listUnik2 = removeDuplicates(inputlist2);
+
+List<String> removeDuplicates(List<String> inputList) {
+  Set<String> uniqueSet = Set<String>.from(inputList);
+  return uniqueSet.toList();}
 //no 2 frekuensi kemunculan data dalam list
 
 List<String> listinput = ['js', 'js', 'js','golang', 'python','js','js','golang','rust']; 
 
 void main(){
-  Map<String, int> frekuensiMap = {};
+ 
+  print(listUnik);
+    print(listUnik2);
+
+map();
+
+}
+
+void map(){
+   Map<String, int> frekuensiMap = {};
 for(String item in listinput){
   if(frekuensiMap.containsKey(item)){
     frekuensiMap[item] = (frekuensiMap[item]?? 0)+ 1;
@@ -19,5 +34,4 @@ for(String item in listinput){
  frekuensiMap.forEach((key, value) {
     print('$key: $value');
   });
-  print(listUnik);
 }
