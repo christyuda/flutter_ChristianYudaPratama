57653496ca37048e3d6ca80ea415134981a61b95
christyuda <yudacihuy09@gmail.com>
2023-08-30 19:52:09 +0700

    Menambahkan final

--- 06_Dart_Object_Oriented_Programming_B/praktikum/soalprioritas1.dart
+++ 06_Dart_Object_Oriented_Programming_B/praktikum/soalprioritas1.dart
@@ -1,35 +1,3 @@
-// class Hewan {
-//   double _berat;
-
-//   Hewan(double berat) : _berat = berat {
-//     if (berat <= 0) {
-//       throw Exception("maaf berat kurang harus melebihi dari 0");
-//     }
-//   }
-//   double get berat => _berat;
-
-//   set berat(double beratbaru) {
-//     if (beratbaru > 0) {
-//       _berat = berat;
-//     } else {
-//       throw Exception("Beratnya harus melebih dari 0 cuy");
-//     }
-//   }
-// }
-
-// void main() {
-//   Hewan Sapi = Hewan(600);
-//   print("berat si sapi adalah: ${Sapi.berat}kg");
-//   Hewan monyet = Hewan(200);
-//   print("berat si monyet itu adalah ${monyet.berat}kg");
-
-//   try {
-//     Sapi.berat = -100;
-//   } catch (e) {
-//     print(e);
-//   }
-// }
-
 
 class Hewan {
   double berat;
@@ -38,14 +6,13 @@ class Hewan {
 }
 
 class Mobil {
-  double kapasitas;
   double muatanBeratMaksimum;
-  List<Hewan> muatan = [];
+  final List<Hewan> muatan = [];
 
-  Mobil(this.kapasitas, this.muatanBeratMaksimum);
+  Mobil(this.muatanBeratMaksimum);
 
   void tambahMuatan(Hewan hewan) {
-    double totalBeratMuatan = muatan.map((h) => h.berat).fold(0, (a, b) => a + b);
+    final double totalBeratMuatan = muatan.map((h) => h.berat).fold(0, (a, b) => a + b);
     if (totalBeratMuatan + hewan.berat <= muatanBeratMaksimum) {
       muatan.add(hewan);
       print("Hewan dengan berat ${hewan.berat} ditambahkan ke dalam muatan.");
@@ -60,15 +27,14 @@ class Mobil {
 }
 
 void main() {
-  Mobil mobil = Mobil(1000, 500); // Membuat objek mobil dengan kapasitas 1000 dan berat maksimum muatan 500
+  Mobil mobil = Mobil(500); 
+  final Hewan hewan1 = Hewan(200);
+  final Hewan hewan2 = Hewan(300); 
+  final Hewan hewan3 = Hewan(150); 
   
-  Hewan hewan1 = Hewan(200); // Membuat hewan dengan berat 200
-  Hewan hewan2 = Hewan(300); // Membuat hewan dengan berat 300
-  Hewan hewan3 = Hewan(150); // Membuat hewan dengan berat 150
+  mobil.tambahMuatan(hewan1); 
+  mobil.tambahMuatan(hewan2); 
+  mobil.tambahMuatan(hewan3); 
   
-  mobil.tambahMuatan(hewan1); // Menambahkan hewan1 ke dalam muatan mobil
-  mobil.tambahMuatan(hewan2); // Menambahkan hewan2 ke dalam muatan mobil
-  mobil.tambahMuatan(hewan3); // Tidak dapat menambahkan hewan3 karena muatan penuh
-  
-  print("Total berat muatan mobil: ${mobil.totalMuatan()}"); // Menampilkan total berat muatan mobil
+  print("Total berat muatan mobil: ${mobil.totalMuatan()}"); 
 }
